730daf7733800d9f3d8a738dfe65fb099678ec54
Arpi Harutyunyan <aharutyunyan@loyhutz.de>
2020-12-16 22:08:43 +0400

    created structure

---
+++ pages/NavBarPages/Forum/Forum.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <div class="forumTextContainer">
+            <div>Это только временный текст в группах</div>
+            <div>Расскажем, как научиться, в сообщениях группы.</div>
+        </div>
+    </div>
+</template>
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+    
+})
+</script>
+<style lang="scss" scoped>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,25 @@
 <template>
+    <div>
+        <Header></Header>
+        <div class="mainContent">
+            <SideBar></SideBar>
+            <Forum></Forum>
+            <RightSection></RightSection>
+        </div>
+    </div>
 </template>
-
 <script lang="ts">
-import Vue from 'vue'
-
-export default Vue.extend({})
+import Vue from 'vue';
+import Header from '../components/Header.vue';
+import SideBar from '../components/SideBar.vue';
+import RightSection from '../pages/NavBarPages/Forum/RightSection.vue';
+import Forum from '../pages/NavBarPages/Forum/Forum.vue';
+export default Vue.extend({
+    components: {
+        Header,
+        SideBar,
+        RightSection,
+        Forum
+    }
+})
 </script>
-
-<style>
-</style>
